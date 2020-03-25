5a4edf6c75d7b174a50eb2b55f7fed7bf2b596ad
Nico-toulouse <nicoramon@hotmail.com>
2020-03-25 16:09:24 +0100

    new_one

---
+++ main.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Louis Collet'));
+console.log(chalk.red('Paul Cartier'));
+console.log(chalk.yellow('Adrien Faure'));
+console.log(chalk.green('Leslie Rouzier'));
